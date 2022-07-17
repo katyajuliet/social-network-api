47229b4d09e9726656fb761dbf2530ee507aebe0
Katya Juliet Lerner <katyajuliet22@gmail.com>
2022-07-16 17:49:10 -0700

    adding dotenv and .gitignore

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const db = require("./config/connection");
 const routes = require("./routes");
 
+// const dotenv = require("dotenv")
+// dotenv.config()
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;

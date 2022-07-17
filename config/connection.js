9d83135ca2ce8ec411f85fda24dcae59fbf28110
Katya Juliet Lerner <katyajuliet22@gmail.com>
2022-07-16 20:38:56 -0700

    changing to not require online mpngo cluster

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,14 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-        } )
-    } catch (err) {
-        console.error(err);
-    }
-}
-// mongoose.connect(
-//   process.env.MONGODB_URI || "mongodb+srv://rootuser:<password>@cluster0.al2rsgk.mongodb.net/?retryWrites=true&w=majority",
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   }
-// );
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-// // Use this to log mongo queries being executed!
-// mongoose.set("debug", true);
+// Use this to log mongo queries being executed!
+mongoose.set("debug", true);
 
-module.exports = connectDB
+module.exports = mongoose.connection;
--- server.js
+++ server.js
@@ -1,23 +1,15 @@
-const dotenv = require("dotenv")
 const express = require("express");
-// const db = require("./config/connection");
+const db = require("./config/connection");
 const routes = require("./routes");
-const mongoose = require("mongoose");
-const connectDB = require('./config/connection');
-
-connectDB();
-// dotenv.config()
-
-const dateFormat = require("../utils/dateFormat");
 
 const app = express();
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-mongoose.connection.once("open", () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });

f85d28858957f557d09f7ba3f573f31152a08ac6
Katya Juliet Lerner <katyajuliet22@gmail.com>
2022-07-18 14:20:37 -0700

    updated server file

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost:27017/social-network-api",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
 
-// Use this to log mongo queries being executed!
-mongoose.set("debug", true);
+// // Use this to log mongo queries being executed!
+// mongoose.set("debug", true);
 
-module.exports = mongoose.connection;
+// module.exports = mongoose.connection;
--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require("express");
-const db = require("./config/connection");
+// const db = require("./config/connection");
 const routes = require("./routes");
+const mongoose = require("mongoose");
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network-api",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.set('debug', true);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-db.once("open", () => {
+
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
   });
-});
+
